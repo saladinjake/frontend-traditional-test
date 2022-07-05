78637d11c2785a419d2d2727ab60fba90f39faa8
saladinjake <juwavictor@gmail.com>
2022-07-05 08:49:56 +0100

    react init

---
+++ react-version/src2/themes/theme.js
@@ -0,0 +1,22 @@
+export default {
+  // Breakpoints for responsive design
+  breakpoints: {
+    sm: 'screen and (max-width: 640px)',
+    md: 'screen and (max-width: 768px)',
+    lg: 'screen and (max-width: 1024px)',
+    xl: 'screen and (max-width: 1280px)'
+  },
+  colors: {
+    primary1: "#000",
+    background1: "#0c0e1b",
+    accent1: "hsl(34.5,97.6%,62.9%)",
+    button: "#fff",
+    background2: "#333",
+  },
+  
+  fonts: {
+    title: "Poppins, sans-serif",
+    main: "Poppins, sans-serif"
+  },
+  
+}
